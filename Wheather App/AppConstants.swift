ce338b73df4045d63fb3b88c1bfc6a64eeed6e80
gisele <gisesonia@gmail.com>
2023-11-02 18:26:15 -0300

    Criando arquivo de nomes de cores e imagens

--- Wheather App/AppConstants.swift
+++ Wheather App/AppConstants.swift
@@ -5,4 +5,15 @@
 //  Created by GISELE TOLEDO on 02/11/23.
 //
 
-import Foundation
+import UIKit
+
+struct AppColors {
+    static let primaryColor = UIColor(named: "primaryColor")
+    // Adicione outras cores aqui
+}
+
+struct AppImages {
+    static let sunIcon = UIImage(named: "sunIcon")
+    // Adicione outras imagens aqui
+}
+
